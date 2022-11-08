c877f0ae3fc88e802dc5512db7e2889bf0592a52
ecmanion <89819521+ecmanion@users.noreply.github.com>
2022-11-08 14:22:50 -0500

    Add files via upload

---
+++ app/app.js
@@ -0,0 +1,230 @@
+var CAMPING = [
+    {
+        campTitle: "Adventure Backpack",
+        campFullImg: "backpack_200.jpg",
+        campLargeImg: "backpack_400.jpg",
+        campBriefDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus labore facilis quo delectus aut dignissimos!",
+        campLongDescription: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique voluptatum vero error tempore nulla voluptas dolor natus. Officia impedit aliquid ullam obcaecati laudantium explicabo earum, fugiat aut harum iste error. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat fuga exercitationem rem fugiat rerum nulla, ullam soluta doloribus blanditiis, itaque sequi ut at mollitia aperiam accusantium maiores perferendis quisquam consequatur!" ,
+        campPrice: "$200",
+        campRating: "4 stars",
+        campMaker: "The North Face",
+        campSize: "One Size",
+        campReleaseDate: "2020",
+        campCustomization: "Yes",
+        campType: "On the go",
+    },
+    {
+        campTitle: "Foldable Chairs",
+        campFullImg: "chairs_200.jpg",
+        campLargeImg: "chairs_400.jpg",
+        campBriefDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus labore facilis quo delectus aut dignissimos!",
+        campLongDescription: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique voluptatum vero error tempore nulla voluptas dolor natus. Officia impedit aliquid ullam obcaecati laudantium explicabo earum, fugiat aut harum iste error. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat fuga exercitationem rem fugiat rerum nulla, ullam soluta doloribus blanditiis, itaque sequi ut at mollitia aperiam accusantium maiores perferendis quisquam consequatur!" ,
+        campPrice: "$50",
+        campRating: "5 stars",
+        campMaker: "REI",
+        campSize: "One Size",
+        campReleaseDate: "2018",
+        campCustomization: "No",
+        campType: "Relaxing",
+    },
+    {
+        campTitle: "Chill Cooler",
+        campFullImg: "cooler_200.jpg",
+        campLargeImg: "cooler_400.jpg",
+        campBriefDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus labore facilis quo delectus aut dignissimos!",
+        campLongDescription: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique voluptatum vero error tempore nulla voluptas dolor natus. Officia impedit aliquid ullam obcaecati laudantium explicabo earum, fugiat aut harum iste error. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat fuga exercitationem rem fugiat rerum nulla, ullam soluta doloribus blanditiis, itaque sequi ut at mollitia aperiam accusantium maiores perferendis quisquam consequatur!" ,
+        campPrice: "$300",
+        campRating: "4.5 stars",
+        campMaker: "Yeti",
+        campSize: "One Size",
+        campReleaseDate: "2015",
+        campCustomization: "Yes",
+        campType: "On the go",
+    },
+    {
+        campTitle: "Firewood",
+        campFullImg: "firewood_200.jpg",
+        campLargeImg: "firewood_400.jpg",
+        campBriefDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus labore facilis quo delectus aut dignissimos!",
+        campLongDescription: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique voluptatum vero error tempore nulla voluptas dolor natus. Officia impedit aliquid ullam obcaecati laudantium explicabo earum, fugiat aut harum iste error. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat fuga exercitationem rem fugiat rerum nulla, ullam soluta doloribus blanditiis, itaque sequi ut at mollitia aperiam accusantium maiores perferendis quisquam consequatur!" ,
+        campPrice: "$40",
+        campRating: "5 stars",
+        campMaker: "Firewood Inc",
+        campSize: "Various",
+        campReleaseDate: "2010",
+        campCustomization: "No",
+        campType: "Cold Weather",
+    },
+    {
+        campTitle: "Tent",
+        campFullImg: "tent_200.jpg",
+        campLargeImg: "tent_400.jpg",
+        campBriefDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus labore facilis quo delectus aut dignissimos!",
+        campLongDescription: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique voluptatum vero error tempore nulla voluptas dolor natus. Officia impedit aliquid ullam obcaecati laudantium explicabo earum, fugiat aut harum iste error. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat fuga exercitationem rem fugiat rerum nulla, ullam soluta doloribus blanditiis, itaque sequi ut at mollitia aperiam accusantium maiores perferendis quisquam consequatur!" ,
+        campPrice: "$200",
+        campRating: "3.8 stars",
+        campMaker: "REI",
+        campSize: "One Size",
+        campReleaseDate: "2016",
+        campCustomization: "Yes",
+        campType: "On the go",
+    },
+    {
+        campTitle: "Marshmallows",
+        campFullImg: "marshmallows_200.jpg",
+        campLargeImg: "marshmallows_400.jpg",
+        campBriefDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus labore facilis quo delectus aut dignissimos!",
+        campLongDescription: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique voluptatum vero error tempore nulla voluptas dolor natus. Officia impedit aliquid ullam obcaecati laudantium explicabo earum, fugiat aut harum iste error. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat fuga exercitationem rem fugiat rerum nulla, ullam soluta doloribus blanditiis, itaque sequi ut at mollitia aperiam accusantium maiores perferendis quisquam consequatur!" ,
+        campPrice: "$5",
+        campRating: "4 stars",
+        campMaker: "Puff",
+        campSize: "One Size",
+        campReleaseDate: "2005",
+        campCustomization: "No",
+        campType: "Relaxing",
+    },
+    {
+        campTitle: "Thermal Mug",
+        campFullImg: "mugs_200.jpg",
+        campLargeImg: "mugs_400.jpg",
+        campBriefDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus labore facilis quo delectus aut dignissimos!",
+        campLongDescription: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique voluptatum vero error tempore nulla voluptas dolor natus. Officia impedit aliquid ullam obcaecati laudantium explicabo earum, fugiat aut harum iste error. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat fuga exercitationem rem fugiat rerum nulla, ullam soluta doloribus blanditiis, itaque sequi ut at mollitia aperiam accusantium maiores perferendis quisquam consequatur!" ,
+        campPrice: "$50",
+        campRating: "5 stars",
+        campMaker: "Hydroflask",
+        campSize: "12 oz, 16oz",
+        campReleaseDate: "2020",
+        campCustomization: "Yes",
+        campType: "On the go",
+    },
+    {
+        campTitle: "Retro Camper",
+        campFullImg: "retroCamper_200.jpg",
+        campLargeImg: "retroCamper_400.jpg",
+        campBriefDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus labore facilis quo delectus aut dignissimos!",
+        campLongDescription: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique voluptatum vero error tempore nulla voluptas dolor natus. Officia impedit aliquid ullam obcaecati laudantium explicabo earum, fugiat aut harum iste error. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat fuga exercitationem rem fugiat rerum nulla, ullam soluta doloribus blanditiis, itaque sequi ut at mollitia aperiam accusantium maiores perferendis quisquam consequatur!" ,
+        campPrice: "$7,000",
+        campRating: "4.7 stars",
+        campMaker: "VW",
+        campSize: "One Size",
+        campReleaseDate: "2020",
+        campCustomization: "Yes",
+        campType: "On the go",
+    },
+    {
+        campTitle: "Silver Camper",
+        campFullImg: "silverCamper_200.jpg",
+        campLargeImg: "silverCamper_400.jpg",
+        campBriefDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus labore facilis quo delectus aut dignissimos!",
+        campLongDescription: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique voluptatum vero error tempore nulla voluptas dolor natus. Officia impedit aliquid ullam obcaecati laudantium explicabo earum, fugiat aut harum iste error. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat fuga exercitationem rem fugiat rerum nulla, ullam soluta doloribus blanditiis, itaque sequi ut at mollitia aperiam accusantium maiores perferendis quisquam consequatur!" ,
+        campPrice: "$9,000",
+        campRating: "4.5 stars",
+        campMaker: "Airstream",
+        campSize: "One Size",
+        campReleaseDate: "2022",
+        campCustomization: "Yes",
+        campType: "On the go",
+    },
+    {
+        campTitle: "Lantern",
+        campFullImg: "light_200.jpg",
+        campLargeImg: "light_400.jpg",
+        campBriefDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus labore facilis quo delectus aut dignissimos!",
+        campLongDescription: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique voluptatum vero error tempore nulla voluptas dolor natus. Officia impedit aliquid ullam obcaecati laudantium explicabo earum, fugiat aut harum iste error. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat fuga exercitationem rem fugiat rerum nulla, ullam soluta doloribus blanditiis, itaque sequi ut at mollitia aperiam accusantium maiores perferendis quisquam consequatur!" ,
+        campPrice: "$70",
+        campRating: "3 stars",
+        campMaker: "REI",
+        campSize: "One Size",
+        campReleaseDate: "2019",
+        campCustomization: "Yes",
+        campType: "On the go",
+    },
+];
+
+function loadData (){
+    $.each(CAMPING, function (idx, camp){
+    $("#app").append(`<div class="container-outside">
+    <div class="container"><div id="${idx}" class="one oneLarger">
+    <div class="campTitle">
+        <h4>${camp.campTitle}</h4> 
+    </div>
+    <div class="campFullImg gameLargeImg">
+        <img src="images/${camp.campFullImg}" alt="${camp.campTitle}">
+    </div>
+    <div class="description description-full">
+        <p>${camp.campBriefDescription}</p>
+    </div>
+    <div class="price">
+        <b>Price:</b> ${camp.campPrice}
+    </div>
+    <div class="rating">
+    <b>Rating:</b> ${camp.campRating}
+    </div>
+    <div class="maker">
+    <b>Maker:</b> ${camp.campMaker}
+    </div>
+    
+    </div>
+</div>
+</div>`);
+    });
+
+    initListeners();
+}
+
+function initListeners () {
+    $(".oneLarger").click(function (e){
+        let campIndex = e.currentTarget.id;
+        console.log(campIndex);
+        $("#app").html(`<div class="container-outside container-outside-full">
+        <div class="container"> <div class="oneLarger one">
+        <div class="titleImg">
+            <div class="campTitleLarge">
+                <h4>${CAMPING[campIndex].campTitle}</h4> 
+            </div>
+            <div class="campFullImg campLargeImg">
+                <img src="images/${CAMPING[campIndex].campLargeImg}" alt="${CAMPING[campIndex].campTitle}">
+            </div>
+            </div>
+            <div class="elements">
+            <div class="description description-full">
+            ${CAMPING[campIndex].campLongDescription}
+            </div>
+            <div class="price">
+            <b>Price: </b>${CAMPING[campIndex].campPrice}
+        </div>
+            <div class="rating">
+            <b>Rating:</b>  ${CAMPING[campIndex].campRating}
+            </div>
+            <div class="maker">
+            <b>Maker:</b> ${CAMPING[campIndex].campMaker}
+            </div>
+            <div class="size">
+        <b>Size: </b> ${CAMPING[campIndex].campSize}
+        </div>
+        <div class="campReleaseDate">
+            <b>Release Date:</b> ${CAMPING[campIndex].campReleaseDate}
+        </div>
+        <div class="customization">
+            <b>Customization: </b>${CAMPING[campIndex].campCustomization}
+        </div>
+        <div class="type">
+            <b>Camping Type: </b>${CAMPING[campIndex].campType}
+        </div> </div></div></div> <div class="close">CLOSE</div>`);
+
+        addCloseListener();
+    });
+}
+
+    function addCloseListener (){
+        $(".close").click(function () {
+            $("#app").html("");
+            loadData();
+        });
+    }
+
+$(document).ready(function(){
+
+    loadData();
+
+});
